8cfab6ce04e1e73b46fbb36e45bf5fb165a11276
Leon  Gunnah <114517374+Sir-Leon1@users.noreply.github.com>
2023-02-09 16:13:05 +0300

    Hello world

---
+++ hello_world.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello world");
+    printf("\nHello world");
+
+    int Age1 = 10;
+    float Average = 20.4444;
+    char Gender = 'F';
+    double decimal = 20.55555555555;
+    char *a = "My name";
+
+    printf("\nThe age is %d", Age1);
+    printf("\nthe average is %f",Average);
+    printf("\nthe gender is %c",Gender);
+    printf("\nthe Average is %lf",decimal);
+    printf("\nthe string is %s",a);
+    return 0;
+}
