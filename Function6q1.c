7f8318db0c9b80dd81a4ac3c51578a684bfb096a
Sir-Leon <gunnahleonagandi@gmail.com>
2023-03-25 08:12:51 +0300

    Question 1

--- Function6q1.c
+++ Function6q1.c
@@ -22,37 +22,47 @@ int main()
     int max_bricks, max_bricks2, max_bricks3;
 
     printf("Enter the following container dimensions\nLength: ");
-    scanf("%f", &cont_len);
+    scanf("%lf", &cont_len);
     printf("Width: ");
-    scanf("%f", &cont_widt);
+    scanf("%lf", &cont_widt);
     printf("Height: ");
-    scanf("%f", &cont_heigh);
+    scanf("%lf", &cont_heigh);
 
 
     max_bricks = len_by_len(cont_len, cont_widt, cont_heigh);
-    printf("\nIf bricks are laid\n"
+    /*printf("\nIf bricks are laid\n"
             "ONLY with lenth of bricks against the\n"
             "length of the container.\n"
             "Maximum number of bricks that can be\n"
             "placed in the container is %d", max_bricks);
+    */
 
 
 
     max_bricks2 = len_by_widt(cont_len, cont_widt, cont_heigh);
-    printf("\n\n\nIf bricks are laid\n"
+    /*printf("\n\n\nIf bricks are laid\n"
             "ONLY with width of brick against the\n"
             "length of the container.\n"
             "Muximum number of bricks that can be\n"
             "placed in the container is %d", max_bricks2);
+    */
 
 
 
     max_bricks3 = len_by_heigh(cont_len, cont_widt, cont_heigh);
-    printf("\n\n\nIf bricks are laid\n"
+    /*printf("\n\n\nIf bricks are laid\n"
             "ONLY with height of bricks against the\n"
             "length of the container.\n"
             "Maximum number of bricks that can be\n"
             "placed in the container is %d", max_bricks3);
+    */
+
+    if ((max_bricks >= max_bricks2) && (max_bricks >= max_bricks3))
+        printf("Maximum number of bricks that can fit ist: %d", max_bricks);
+    else if ((max_bricks2 >= max_bricks) && (max_bricks2 >= max_bricks3))
+        printf("Maximum number of bricks that can fit isu: %d", max_bricks2);
+    else
+        printf("Maximum number of bricks that can fit isv: %d", max_bricks3);
 
     return (0);
 }
@@ -73,10 +83,11 @@ int len_by_len(double len, double widt, double heigh)
     int case_1, case_2;
 
     case_1 = ((len/brick_len) * (widt/brick_widt) * (heigh/brick_heigh));
+    printf("%d", case_1);
 
     case_2 = ((len/brick_len) * (widt/brick_heigh) * (heigh/brick_widt));
 
-    if ((case_1 > case_2) || (case_1 == case_2))
+    if ((case_1 >= case_2))
         return (case_1);
     else
         return (case_2);
@@ -102,7 +113,7 @@ int len_by_widt(double len, double widt, double heigh)
 
     case_2 = ((len/brick_widt) * (widt/brick_heigh) * (heigh/brick_len));
 
-    if ((case_1 > case_2) || (case_1 == case_2))
+    if ((case_1 >= case_2))
         return (case_1);
     else
         return (case_2);
@@ -127,7 +138,7 @@ int len_by_heigh(double len, double widt, double heigh)
 
     case_2 = ((len/brick_heigh) * (widt/brick_widt) * (heigh/brick_len));
 
-    if ((case_1 > case_2) || (case_1 == case_2))
+    if ((case_1 >= case_2))
         return (case_1);
     else
         return (case_2);
