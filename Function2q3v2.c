f0693a6b1f91dd45147ee2e099d779c4d83b3a76
Sir-Leon <gunnahleonagandi@gmail.com>
2023-03-27 22:06:20 +0300

    Question 3

--- Function2q3v2.c
+++ Function2q3v2.c
@@ -10,31 +10,31 @@ int main()
 {
     float radius, height, total_volume, surface_area, total_cost, result, side;
 
-    printf("Enter the dimensions of the cone\nRadius=");
+    printf("Enter the following dimensions of the cone\nRadius=");
     scanf("%f", &radius);
-    printf("\nHeight=");
+    printf("Height=");
     scanf("%f", &height);
     
     if ((radius <= 0) || (height <= 0))
         {
-        printf("Invalid input.Please input correct parameters");
-        return (0);
+            printf("Invalid input.Please input correct parameters");
+            return (0);
         }
   
 
 
-    radius = radius * 0.025;
-    height = height * 0.025;
+    radius = radius * 0.025; //converts radius to metres
+    height = height * 0.025; //converts height to metres
 
     total_volume = volume(radius, height);
     side = cbrtf(total_volume);
     surface_area = area(side);
     total_cost = cost(surface_area, 150);
 
-    printf("\nThe volume is = %fm³", total_volume);
-    printf("\nThe surface area is = %fm²", surface_area);
-    printf("\nThe dimensions of the cube are\nLength: %fm\nWidth: %fm\nHeight: %fm", side, side, side);
-    printf("\nThe total cost of printing will be = KSH%f", total_cost);
+    printf("\nThe volume of the cone is = %.3fm³", total_volume);
+    printf("\nThe surface area of the cube formed from cone = %.3fm²", surface_area);
+    printf("\nThe dimensions of the cube are\nLength: %.3fm\nWidth: %.3fm\nHeight: %.3fm", side, side, side);
+    printf("\nThe total cost of printing will be = KSH%.2f", total_cost);
 
     return (0);
 }
